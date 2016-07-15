3e7a85fa80faec07fed55663f51dd318cca42e07
wmh111123456 <373664138@qq.com>
2016-07-15 16:08:29 +0800

    init

---
+++ BusinessLayer/BusinessLayer/Note.h
@@ -0,0 +1,13 @@
+//
+//  Note.h
+//  BusinessLayer
+//
+//  Created by tiger on 16/7/15.
+//  Copyright © 2016年 buystreet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Note : NSObject
+
+@end
